f9532aadbaa160837cebba75fd80d86cd7bb0d42
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2024-07-26 23:34:25 +0100

    refact: change class to functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,34 +1,33 @@
-export class Utils{
 
-    buildRoutePath(path)
-    {
-        const parametersRegex = /:([a-zA-Z]+)/g;
-        const pathWithParams  = path.replace(parametersRegex, '(?<$1>[a-z0-9\-_]+)');                                
-        const pathRegex       = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
-        return pathRegex;
-    }
 
-    extractQueryParams(query)
-    {
-        query = query.substr(1);
-        query = query.split('&');
-        const queryParams = query.reduce((queryParams, param) =>{
-            const [key, value] = param.split('=');
-            queryParams[key] = value;
-            return queryParams;
-        }, {});
+export function buildRoutePath(path)
+{
+    const parametersRegex = /:([a-zA-Z]+)/g;
+    const pathWithParams  = path.replace(parametersRegex, '(?<$1>[a-z0-9\-_]+)');                                
+    const pathRegex       = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+    return pathRegex;
+}
 
-        return queryParams;
-    }
+export function extractQueryParams(query)
+{
+    query = query.substr(1);
+    query = query.split('&');
+    const queryParams = query.reduce((queryParams, param) =>{
+    const [key, value] = param.split('=');
+    queryParams[key] = value;
+    return queryParams;
+}, {});
 
-    now(){
-        const now     = new Date();
-        const year    = now.getFullYear();
-        const month   = String(now.getMonth() + 1).padStart(2, '0');
-        const day     = String(now.getDate()).padStart(2, '0');
-        const hours   = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;        
-    }
+return queryParams;
+}
+
+export function now(){
+    const now     = new Date();
+    const year    = now.getFullYear();
+    const month   = String(now.getMonth() + 1).padStart(2, '0');
+    const day     = String(now.getDate()).padStart(2, '0');
+    const hours   = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;        
 }
